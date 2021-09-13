107f1e754787454c38cb4e83d9a6b7b6ad9456fa
Dan Kuzma <>
2021-09-13 09:50:47 -0400

    Initial commit - project created

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,25 @@
+
+
+/**
+ *
+ * @author dkuzm
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        
+        int x=0 y=10;
+        
+        do{
+          
+            System.out.println("i = "+x');
+            
+        }while(x!=10) ;
+            
+        }
+    }
+   
+
+
+
+       
